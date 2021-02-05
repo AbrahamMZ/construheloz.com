89002f1b0b9cc28a845bf13358c062c22db46849
Abraham Martinez <mga.abraham@gmail.com>
2021-02-05 14:48:54 -0600

    add phone

--- src/components/base/BusinessContact.vue
+++ src/components/base/BusinessContact.vue
@@ -48,11 +48,11 @@
           title: 'Address',
           text: 'Manuel M. Ponce 1108 León Moderno 37480 León, Guanajuato',
         },
-        // {
-        //   icon: 'mdi-phone',
-        //   title: 'Phone',
-        //   text: '<a href="tel:4773954427">(477)395-44-27</a>',
-        // },
+        {
+          icon: 'mdi-phone',
+          title: 'Phone',
+          text: '<a href="tel:4776889478">(477)688-94-78</a>',
+        },
         {
           icon: 'mdi-email-outline',
           title: 'Email',
